eee0a89884ca9a29c1130d355fb5f8a31effb11e
tashian <carl@tashian.com>
2016-07-05 13:42:45 -0700

    Minor refactor and cleanup

--- background.js
+++ background.js
@@ -1,43 +1,85 @@
 // Chrome automatically creates a background.html page for this to execute.
 // notify of page refreshes
 // Background page -- background.js
+// "Listening to events"
+// Ambient backdrop to software dev
+// Office space sound -- code checked in today
+// DevOps sound
+// images and sound vs. js vs. css
+// XSS (how much is this site tracking me?)
 
-var bass = new Wad({
-    source : 'sine',
-    env : {
-        attack : .02,
-        decay : .1,
-        sustain : .9,
-        hold : .4,
-        release : .1
-    },
-    loop: true
-});
+sound = new function() {
+  var tones = {
+    bass: new Wad({
+            source : 'sine',
+            env : {
+                attack : .02,
+                decay : .1,
+                sustain : .9,
+                // hold : .4,
+                release : .1
+            }
+        }),
+    edgy: new Wad({
+            source : 'square',
+            volume: 0.1,
+            env : {
+                attack : .02,
+                decay : .1,
+                sustain : .9,
+                hold : .2,
+                release : .1
+            }
+        })
+  }
 
-function newBass(iteration, label) {
+  var currentlyPlaying = {};
   var notes = ['C3','E3','G3','B3','D4','F#4','A4'];
-  var currentNote = iteration % notes.length;
-  bass.play({ pitch : notes[currentNote], label: label });
-}
 
-function muteUnmute() {
-  console.log(bass.volume);
-  if (bass.volume > 0) {
-    console.log('muted');
-    bass.setVolume(0);
-  } else {
-    console.log('unmuted');
-    bass.setVolume(initialVol);
-  }
-}
+  this.muted = false;
 
-function resetAll() {
-  console.log('reset');
-  requestCount = 0;
-  for (var i = 0; i < oscs.length; i++) {
-    bass.stop(oscs[i]);
+  this.isPlaying = function(label) {
+    if (currentlyPlaying.hasOwnProperty(label)) { return true; }
+    return false;
   }
-  oscs = [];
+
+  this.canPlay = function(label) {
+    if (this.isPlaying(label)) { return false; }
+    if (this.muted) { return false; }
+    return true;
+  }
+
+  this.playLongNote = function(toneName, label) {
+    if (!this.canPlay(label)) { return; }
+    var currentNote = Object.keys(currentlyPlaying).length % notes.length;
+    currentlyPlaying[label] = toneName;
+    tones[toneName].play({ pitch : notes[currentNote], label: label });
+  }
+
+  this.play = this.playLongNote;
+
+  this.playShortNote = function(toneName, label) {
+    if (!this.canPlay(label)) { return; }
+    tones[toneName].play({pitch: notes[0], label: label});
+  }
+
+  this.stop = function(label) {
+    if (!this.isPlaying(label)) { return; }
+    tones[currentlyPlaying[label]].stop(label);
+    delete currentlyPlaying[label];
+  }
+
+  this.stopAll = function() {
+    console.log('reset');
+    for (var i = 0; i < currentlyPlaying.length; i++) {
+      this.stop(currentlyPlaying[i]);
+    }
+  }
+
+  this.muteUnmute = function() {
+    this.stopAll();
+    this.muted = !this.muted;
+  }
 }
 
 chrome.runtime.onConnect.addListener(function(popupConnection) {
@@ -46,9 +88,9 @@ chrome.runtime.onConnect.addListener(function(popupConnection) {
   var popupCommandListener = function(msg) {
     console.log(msg);
     if (msg.type == 'mute') {
-      muteUnmute();
+      sound.muteUnmute();
     } else {
-      resetAll();
+      sound.stopAll();
     }
   };
 
@@ -60,28 +102,10 @@ chrome.runtime.onConnect.addListener(function(popupConnection) {
 
 });
 
-var requestCount = 0;
-var oscs = [];
-var addAndPlay = function(details) {
-  if (!oscs.includes(details.requestId)) {
-    newBass(requestCount, details.requestId);
-    oscs.push(details.requestId);
-    requestCount++;
-  }
-}
-
-var stopPlaying = function(details) {
-  bass.stop(details.requestId);
-}
-
-chrome.tabs.onUpdated.addListener(function(tabId , info) {
-    if (info.status == "complete") {
-      resetAll();
-    }
-});
-
 chrome.webRequest.onSendHeaders.addListener(
-  addAndPlay,
+  function(details) {
+    sound.play('bass', details.requestId);
+  },
   { urls: [
     "http://*/*",
     "https://*/*"
@@ -89,7 +113,12 @@ chrome.webRequest.onSendHeaders.addListener(
 );
 
 chrome.webRequest.onErrorOccurred.addListener(
-  stopPlaying,
+  function(details) {
+    console.log('error');
+    console.log(details);
+    sound.stop(details.requestId);
+    sound.playShortNote('edgy', details.requestId);
+  },
   { urls: [
     "http://*/*",
     "https://*/*"
@@ -97,7 +126,9 @@ chrome.webRequest.onErrorOccurred.addListener(
 );
 
 chrome.webRequest.onCompleted.addListener(
-  stopPlaying,
+  function(details) {
+    sound.stop(details.requestId);
+  },
   { urls: [
     "http://*/*",
     "https://*/*"
