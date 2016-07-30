43ed0e5ab14422b91bb22f3363c86b39045e60d0
tashian <carl@tashian.com>
2016-07-30 13:20:57 -0700

    Make the link to my site (in the popup) work properly

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,5 +1,13 @@
 var muted = false;
 
+// Make links open into new tabs -- otherwise they don't work.
+$(document).ready(function(){
+   $('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
+});
+
 document.querySelector("#mute").addEventListener('click', muteUnmute);
 
 function muteUnmute() {
